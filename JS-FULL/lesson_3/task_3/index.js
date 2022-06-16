01be77ad238cc6c266eccb5b535e1756e47bcfa2
VitaliiHryb <vitaliigryb@ukr.net>
2022-06-16 16:31:40 +0300

    JS L3 T3

--- JS-FULL/lesson_3/task_3/index.js
+++ JS-FULL/lesson_3/task_3/index.js
@@ -1,23 +1,43 @@
-/*const apple = "Apple";
-const anotherApple = "Apple";
-const house = "17 Wall st.";
-const anotherHouse = "17 Wall st.";*/
+/* eslint-disable */
 
-const goodNumber = 17;
-let anotherNumber = goodNumber;
-anotherNumber = 77;
+/* Результатом сравнения является булевое значение */
 
-console.log(goodNumber);
-console.log(anotherNumber);
+/* Сравнение чисел */
+const a = 17;
+const b = 5;
+console.log('a > b:', a > b);
+console.log('a <= b:', a <= b);
+console.log('a == b:', a == b);
+console.log('a != b:', a != b);
 
-const customer = {
-    name: "John",
-    age: 17,
-};
+/* Сравнение строк */
+console.log("'a' < 'b':", 'a' < 'b');
+console.log("'ab' > 'a':", 'ab' > 'a');
 
-const anotherCustomer = customer;
+/* При сравнении переменных разных типов они преобразуются в числа */
+console.log("'17' > 1:", '17' > 1);
 
-anotherCustomer.age = 20;
+/* Строгое сравнение */
+console.log('17 === 1:', 17 === 1);
+console.log('17 === 17:', 17 === 17);
+console.log("17 === '17':", 17 === '17');
+console.log('17 === true:', 17 === true);
+console.log("'0' === '':", '0' === '');
+console.log('true === false:', true === false);
+console.log('true === true:', true === true);
+console.log('null === undefined:', null === undefined);
+console.log('false === 0:', false === 0);
 
-console.log(customer.age);
-console.log(anotherCustomer.age);
+/* Нестрогое сравнение */
+console.log("17 == '17':", 17 == '17');
+console.log("'0' == '':", '0' == '');
+console.log("0 == '':", 0 == '');
+console.log('null == undefined:', null == undefined);
+console.log('false == 0:', false == 0);
+
+/* Осторожно c null и undefined */
+console.log('undefined == null:', undefined == null);
+console.log('undefined == 0:', undefined == 0);
+console.log('null == 0:', null == 0);
+console.log('undefined < 0:', undefined < 0);
+console.log('undefined > 0:', undefined > 0);
