ce23c29c0458a2f60516bd64bedb2d307e1dd670
VitaliiHryb <vitaliigryb@ukr.net>
2022-06-16 14:01:15 +0300

    JS Fix

--- JS-FULL/lesson_2/task_1/index.js
+++ JS-FULL/lesson_2/task_1/index.js
@@ -1,9 +1,5 @@
-/*
-const User Name = 'John';
-const 1greeting = "I'm John";
-const ho%b@by = 'Walking';
-const FavouriteDESSERT = 'cake';
-const super&Number = 17;
-*/
-
-const personName = "John";
+const userName = "John";
+const greeting = "I'm John";
+const hobby = "Walking";
+const favouriteDessert = "cake";
+const superNumber = 17;
