c244377a4d79f8bb3bb78a405943d7c2b855834d
VitaliiHryb <vitaliigryb@ukr.net>
2022-06-16 18:03:10 +0300

    JS L3 T5

--- JS-FULL/lesson_3/task_5/index.js
+++ JS-FULL/lesson_3/task_5/index.js
@@ -1,10 +1,40 @@
-const a = 4;
-const b = 2;
+/* eslint-disable */
 
-console.log("a:", a);
-console.log("b:", b);
+if (true) console.log("I'm in");
 
-console.log("a - b =", a - b);
-console.log("a + b =", a + b);
-console.log("a / b =", a / b);
-console.log("a * b =", a * b);
+if (false) {
+    console.log("I'm not in");
+}
+
+if ('text') {
+    console.log('Yes');
+}
+
+if (null) {
+    console.log('Yes');
+} else {
+    console.log('Else');
+}
+
+// create age variable here
+
+let age;
+age = 18;
+let amount;
+amount = 2;
+
+if (age < 16) {
+    console.log('Too young');
+} else if (age >= 16 && age < 18) {
+    console.log('Almost ready');
+} else if (age >= 18 && age < 65) {
+    console.log('Ready');
+} else {
+    console.log('Else');
+}
+
+// Тернаный оператор ? : ( const result = условие ? значение1 : значение2; )
+
+// create amount variable here
+
+const result = amount > 17 ? 'full' : 'empty';
