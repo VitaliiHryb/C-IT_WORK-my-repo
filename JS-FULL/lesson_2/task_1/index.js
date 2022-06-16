63d80dffbb65d14510dcd363003f2dad09b094d9
VitaliiHryb <vitaliigryb@ukr.net>
2022-06-16 13:27:27 +0300

    JS-FULL L2

---
+++ JS-FULL/lesson_2/task_1/index.js
@@ -0,0 +1,9 @@
+/*
+const User Name = 'John';
+const 1greeting = "I'm John";
+const ho%b@by = 'Walking';
+const FavouriteDESSERT = 'cake';
+const super&Number = 17;
+*/
+
+const personName = "John";
